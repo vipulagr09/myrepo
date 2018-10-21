8be9e1e9c0a74fccb62a3aeb12a7307db32d5f80
Vipul <vipulagr@hotmail.com>
2018-10-21 13:35:28 +0530

    adding the first project to git

---
+++ TestProject/src/main/java/com/test/mavenproj/TestProject/App.java
@@ -0,0 +1,13 @@
+package com.test.mavenproj.TestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
